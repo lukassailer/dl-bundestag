b9831f84ef6dbc8d64507aaf604111aa73a22795
Lukas Sailer <lukas.sailer@meisterplan.com>
2022-01-30 13:24:56 +0100

    Style bar plots - Move numbers above bars - Add colors - Remove legend and add ylabel instead

---
+++ exp/util/colors.py
@@ -0,0 +1,10 @@
+# Note: These might not be the official colors (they are from https://colorganize.com/palette/179)
+
+# alphabetically ordered after full party Name)
+# AfD, GRÜNE, CDU/CSU, LINKE, FDP, SPD
+sixParties = ['#009ee0', '#46962b', '#000000', '#b61c3e', '#ffed00', '#e3000f']
+
+# CDU and CSU seperate
+# AfD, GRÜNE, CDU, CSU, LINKE, FDP, SPD
+sevenParties = ['#009ee0', '#46962b', '#32302e',
+                '#0080c9', '#b61c3e', '#ffed00', '#e3000f']
