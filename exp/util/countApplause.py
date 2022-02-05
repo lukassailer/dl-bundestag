d0e018b9d0e166ba5d1115e934ff21ebc587fb3e
lukassailer <lukassailer@web.de>
2022-02-05 10:29:44 +0100

    Make fundtion documentation appear in preview

--- exp/util/countApplause.py
+++ exp/util/countApplause.py
@@ -1,18 +1,17 @@
 from typing import List
 import pandas as pd
 
-"""
-Count applause given by each party for a list of comments
-
-input:
-    commentList: a list of comments
-
-output:
-    pandas DataFrame linking each party to the times they applauded
-"""
-
 
 def countApplause(commentList: List[str]) -> pd.DataFrame:
+    """
+    Count applause given by each party for a list of comments
+
+    input:
+        commentList: a list of comments
+
+    output:
+        pandas DataFrame linking each party to the times they applauded
+    """
     # matchers are minimal to also match other forms (e.g. genitive)
     partyMatchers = ["AfD", "GRÜNE", "CDU/CSU", "LINKE", "FDP", "SPD"]
 
--- exp/util/getComments.py
+++ exp/util/getComments.py
@@ -1,21 +1,20 @@
 from typing import List
 import pandas as pd
 
-"""
-get comments for dataframe
-comments can be filtered by speaker or party name
-
-input:
-    - dataFrame: a pandas DataFrame with rows "sentences", "main-speaker" and "main-speaker-party"
-    - speakerFilter: speaker name to filter speeches by
-    - partyFilter party name to filter speeches by
-
-output:
-    list of comments
-"""
-
 
 def getComments(dataFrame: pd.DataFrame, speakerFilter: str = None, partyFilter: str = None) -> List[str]:
+    """
+    get comments for dataframe
+    comments can be filtered by speaker or party name
+
+    input:
+        - dataFrame: a pandas DataFrame with rows "sentences", "main-speaker" and "main-speaker-party"
+        - speakerFilter: speaker name to filter speeches by
+        - partyFilter party name to filter speeches by
+
+    output:
+        list of comments
+    """
     comments = []
     for (sentences, speaker, party) in zip(dataFrame["sentences"], dataFrame["main-speaker"], dataFrame["main-speaker-party"]):
         if (speakerFilter is None or speaker == speakerFilter) and (partyFilter is None or party == partyFilter):
